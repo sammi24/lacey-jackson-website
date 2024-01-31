f4f9388c6cbb3690e2e6d64a7346bbaa7297a718
sammi ghazzzawi <swghazzawi@gmail.com>
2024-01-30 21:41:51 -0700

    add todo for navbar and background texture for website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
+<!-- TODO: homepage - add navbar and background texture -->
+
+<h1>Homepage</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+

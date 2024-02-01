4a69d3ef2363e773192fa624ead9a0d82b8ed7fe
sammi ghazzzawi <swghazzawi@gmail.com>
2024-02-01 01:55:32 -0700

    navigation bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<!-- TODO: add navbar and background texture -->
+
+<script>
+	import './styles/layout.scss';
+	import './styles/global.scss';
+</script>
+
+<!-- Navbar -->
+<div class="container flex">
+	<nav class="flex">
+		<ul class="flex">
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/music">Music</a>
+			</li>
+			<li>
+				<a href="/store">Store</a>
+			</li>
+		</ul>
+	</nav>
+</div>
+
+<!-- Footer -->
+<slot />
